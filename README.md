fc5e44a461ab204694ddd9014cf6339dc604fd37
outofhadoop <tianyu.han@aliyun.com>
2018-10-08 21:57:48 +0800

    add a new branch named "hanty"

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # niubi project
 
 斗图制作小程序
+
+### 注释请写的规范！请参考https://github.com/fex-team/styleguide/blob/master/javascript.md。（如果有更好的规范请用你的非常ugly的手敲在这里。不谢）
+
+大佬，每一行都写注释吧，不然你的代码高（鸡）深（吧）莫（玩）测（意），我太菜，看不懂怎么办。
+
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,14 +5,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    _canvas: {
+      w: 100,
+      h: 100
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getUserInfo();
+    this.setCanvasWH();
+    
   },
 
   /**
@@ -62,5 +67,72 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  /**
+   * @name getUserInfo
+   * @description Get user Authorization, 1.uer authorization, 2.save to system album
+   */
+  getUserInfo () {
+    // Gets the user's current settings.
+    // Only the permissions that the applet has requested from the user will appear in the return value.
+    wx.getSetting({
+      success: (authSetting)=>{
+
+        // get user info authorization
+        if (!!!authSetting.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            withCredentials: 'true',
+            lang: 'zh_CN',
+            timeout:10000,
+            success: (result)=>{
+              console.log(' get user information success:', result);
+            },
+            fail: ()=>{},
+            complete: ()=>{}
+          });
+        }
+        // get save to system album authorization 
+        if (!!!authSetting.authSetting['scope.writePhotosAlbum']) {
+          wx.authorize({
+            scope: 'scope.writePhotosAlbum',
+            success: (res)=>{
+              console.log(res);
+            },
+            fail: ()=>{},
+            complete: ()=>{}
+          });
+        }
+        console.log(authSetting);
+        console.log('get user infomation authorization', authSetting.authSetting['scope.userInfo']);
+        console.log('get user infomation authorization', authSetting.authSetting['scope.writePhotosAlbum'] || false);
+      },
+      fail: ()=>{
+        console.log("getSetting error");
+      },
+      complete: ()=>{
+        console.log("getSetting completed");
+      }
+    });
+  },
+  /**
+   * @name setCanvasWH
+   * @description get system infomation
+   * 
+   */
+  setCanvasWH () {
+    wx.getSystemInfo({
+      success: (result)=>{
+        console.log(result);
+        // set the inner canvas height and width
+        this.setData({
+          _canvas: {
+            w: result.windowWidth,
+            h: result.windowHeight
+          }
+        });
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
   }
 })
